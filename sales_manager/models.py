fe237ba2c7f660342e8bf790f5c08f90363f7a4c
Kirill <kirjucha92@gmail.com>
2021-05-19 21:29:06 +0300

    create Model Book view for all books and for book detail

--- sales_manager/models.py
+++ sales_manager/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=100,
+        db_index=True,
+        verbose_name="название",
+    )
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_publish = models.DateField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+
--- sales_manager/urls.py
+++ sales_manager/urls.py
@@ -3,5 +3,6 @@ from sales_manager import views
 
 
 urlpatterns = [
+    path("book_detail/<int:book_id>/", views.book_detail, name="book-detail"),
     path("", views.main_page)
 ]
